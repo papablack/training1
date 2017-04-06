fddaf158307444f0107d74665b8ae1d54b226018
Michał Haracewiat <m.haracewiat@gmail.com>
2017-04-06 15:18:12 +0200

    Logged and not logged tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,20 +3,11 @@
 namespace Tests\AppBundle\Controller;
 
 use AppBundle\DataFixtures\ORM\Users;
+use AppBundle\Entity\User;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-//
-//    public function testIndex()
-//    {
-//        $client = $this->makeClient();
-//
-//        $crawler = $client->request('GET', '/note');
-//
-//        $this->assertEquals(200, $client->getResponse()->getStatusCode());
-//        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
-//    }
 
     public function testIndexNotLogged()
     {
@@ -27,6 +18,25 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/note');
 
         $this->assertStatusCode(302, $client);
+    }
+
+    public function testIndexLogged()
+    {
+        $fixtures = $this->loadFixtures(array(Users::class))->getReferenceRepository();
+
+        /** @var User $user1 */
+        $user1 = $fixtures->getReference('training1');
+
+
+        $client = $this->makeClient(array(
+            'username'=> $user1->getUsername(),
+            'password' => 'polcode',
+        ));
+
+        $crawler = $client->request('GET', '/note/');
+
+        $this->assertStatusCode(200, $client);
+        $this->assertContains('No notes', $crawler->filter('p')->text());
     }
 
 }
